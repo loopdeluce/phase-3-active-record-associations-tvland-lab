dc42fb466461ecbcf1280094a6b092e53c07e110
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-07-05 10:38:54 -0600

    commit tree c8a3610c0ba19ba9e7820f584997b3e8213887e2

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
+require 'Pry'
+
 class Actor < ActiveRecord::Base
   
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      binding.pry
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+
+
 end
+
+# peter = Actor.create(first_name: "Peter", last_name: "Dinklage")
+# thrones = Show.create(name: "Game of Thrones")
+# tyrion = Character.create(name: "Tyrion Lannister", actor_id: peter.id, show_id: thrones.id)
+
+# peter.list_roles
